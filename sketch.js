bbbe40328fc20ebbc79b32c1675b59b90759e137
Atharv1278 <86914569+Atharv1278@users.noreply.github.com>
2021-08-19 12:25:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var astronot , bath , bru , drink , eat , gym , bg , move,  sleeping 
+
+function preload(){
+  bath=loadAnimation("images/bath1.png","images/bath2.png")
+
+}
+
+function setup() {
+  createCanvas(400, 400);
+  //astronot=createSprite(300,230)
+ // astronot.addImage("sleeping ", sleep)
+ // astronot.scale=0.1
+}
+
+function draw() {
+  background(220);
+
+  drawSprite()
+  
+}
+
